ba1634d5dfc29af1eb520971ed1e8abebe2f9b6d
Helene Selmer Nielsen <eaahsn@students.eaaa.dk>
2018-10-11 20:17:29 +0200

    um

---
+++ man05/man05/Controllers/ContactFormController.cs
@@ -0,0 +1,62 @@
+﻿using man05.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace man05.Controllers
+{
+    public class ContactFormController : SurfaceController
+    {
+        public ActionResult Index()
+        {
+            return PartialView("contactForms", new ContactForm());
+        }
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            
+            //if (!ModelState.IsValid)
+            //{
+            //    try {
+                var body = "<p>Email from" + model.Email + "<br />" + model.Name + "<br />" + "besked: " + model.Message + "</p>";
+                var message = new MailMessage();
+
+                message.To.Add("*******e@gmail.com");               
+                message.From = new MailAddress(model.Email);
+                message.Subject = model.subject;
+                message.Body = model.Message + "\n my mail is: " + model.Email;
+                message.IsBodyHtml = true;
+
+                    using (var smtp = new SmtpClient())
+                    {
+                        var credential = new System.Net.NetworkCredential("********@gmail.com", "*******");
+                        smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        smtp.UseDefaultCredentials = false;
+                        smtp.EnableSsl = true;
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.Port = 587;
+                        smtp.Credentials = credential;
+                        smtp.EnableSsl = true;
+                        smtp.Send(message);
+
+                        ViewBag.Status = "Email Sent Successfully.";
+                  }
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        ViewBag.Status = e;
+            //    }
+            //    return CurrentUmbracoPage();
+
+            //}
+         
+     
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ man05/man05/ViewModels/ContactForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace man05.ViewModels
+{
+    public class ContactForm
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string subject { get; set; }
+        public string Message { get; set; }
+
+    }
+}
